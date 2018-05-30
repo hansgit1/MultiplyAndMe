a5431cc98efa95fcbacc6b19391bf71de4da1ef5
Ramon Arents <s1102867@student.hsleiden.nl>
2018-05-30 16:03:24 +0200

    comments verwijderd

--- app/src/main/java/com/example/hans/multiplyandme/MainActivity.java
+++ app/src/main/java/com/example/hans/multiplyandme/MainActivity.java
@@ -4,13 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    // een comment 14:35
-    //nieuwe comment
-    // voor de zekerheid
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test 
+
     }
 }
