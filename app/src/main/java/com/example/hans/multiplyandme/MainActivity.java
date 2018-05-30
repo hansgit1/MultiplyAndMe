e38111a4559a2277b4b917de9fcf997148f3c42d
Hans Katib <katibhans@gmail.com>
2018-05-30 14:55:48 +0200

    Een nieuwe comment is toegevoegd om te checken of kan worden gepullt.

--- app/src/main/java/com/example/hans/multiplyandme/MainActivity.java
+++ app/src/main/java/com/example/hans/multiplyandme/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     // een comment 14:35
     //nieuwe comment
+    // voor de zekerheid
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
