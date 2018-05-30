bd1ce9b510175eb6f7db55a302e7a72f0fdb0a6d
Ramon Arents <s1102867@student.hsleiden.nl>
2018-05-30 16:40:12 +0200

    headerbar verwijderd

--- app/src/main/java/com/example/hans/multiplyandme/MainActivity.java
+++ app/src/main/java/com/example/hans/multiplyandme/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.hans.multiplyandme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
     @Override
