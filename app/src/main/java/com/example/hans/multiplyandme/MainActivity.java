1fb3beff78d63974c1b117f220cf001620acb0e7
Ramon Arents <s1102867@student.hsleiden.nl>
2018-05-30 17:05:40 +0200

    buttons toegevoegd startscherm

--- app/src/main/java/com/example/hans/multiplyandme/MainActivity.java
+++ app/src/main/java/com/example/hans/multiplyandme/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.hans.multiplyandme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+//    Button buttons = (Button) findViewById(R.id.buttons);
+//    Button instellingen = (Button) findViewById(R.id.instellingen);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
