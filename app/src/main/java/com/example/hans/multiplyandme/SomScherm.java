e57d99772cfb53ee403bf40f20e8eef6895a23c2
Ramon Arents <s1102867@student.hsleiden.nl>
2018-06-08 16:34:29 +0200

    pair programming: random waardes aan de buttons gegeven, achtergrondkleur veranderd op basis van antwoord, hoeveel goed of fout iemand heeft bijgehouden met een teller, next button toegevoegd, sommen gaan tot max 10 en gaat dan naar resultaatscherm

--- app/src/main/java/com/example/hans/multiplyandme/SomScherm.java
+++ app/src/main/java/com/example/hans/multiplyandme/SomScherm.java
@@ -1,8 +1,11 @@
 package com.example.hans.multiplyandme;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,38 +13,171 @@ import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class SomScherm extends AppCompatActivity {
-
+    // initialize de ui componenten
     TextView som;
     Button optie1;
     Button optie2;
     Button optie3;
-
+    Button next;
+    //iniatileze variabelen
+    int aantalgoed = 0;
+    int aantalfout = 0;
+    int value;
+    int antwoord;
+    int currentNumber = 1;
+    //oncreate functie
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_som_scherm);
-
+        // iniatilize id's
         som = (TextView) findViewById(R.id.som);
         optie1 = (Button) findViewById(R.id.optie1);
         optie2 = (Button) findViewById(R.id.optie2);
         optie3 = (Button) findViewById(R.id.optie3);
+        next = (Button) findViewById(R.id.next);
 
+        //roep methode aan voor berekening
         calcualteSom((int) (Math.random() * 10));
+        //volgende knop, deze is alleen zichtbaar na invullen antwoord
+        next.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // blijf tot 10 keer doorgaan
+                if(currentNumber < 10){
+                    currentNumber++;
+                    //reset de oude variabelen
+                    optie1.setEnabled(true);
+                    optie2.setEnabled(true);
+                    optie3.setEnabled(true);
+                    optie1.setBackgroundResource(android.R.drawable.btn_default);
+                    optie2.setBackgroundResource(android.R.drawable.btn_default);
+                    optie3.setBackgroundResource(android.R.drawable.btn_default);
+                    //blijf de methode aanroepe zolang currentNumber geen 10 is vanwege 10 sommen
+                    calcualteSom((int) (Math.random() * 10));
+                    if (currentNumber == 10) {
+                        // als de 10de som bereikt is start resultaat activity
+                        startActivity(new Intent(SomScherm.this, Resultaat.class));
+                    }
+                }
+
+            }
+
+        });
     }
 
     //methode om som te berekenen
     public void calcualteSom(int number) {
 
+
         // loop tot de tafel van 10
         for (int i = 1; i <= 10; i++) {
-            int value = i * number;
+            // bereken de som
+            value = i * number;
+            // bepaal random waardes om een random som te generen
             int random1 = (int) (Math.random() * i);
             int random2 = (int) (Math.random() * number);
+            //geef random waardes aan de buttons 10 = max, 1 = min
+            int max = 10;
+            int min = 1;
+            int randombutton1 = ((int) (Math.random() * (max - min))) + min;
+            int randombutton2 = ((int) (Math.random() * (max - min))) + min;
+
+
+            //random kan geen 0 zijn, dus tel er 1 bij op als er 0 uitkomt
             if(random1 == 0 || random2 == 0){
                 random1 = random1 + 1;
                 random2 = random2 + 1;
             }
+            // voeg de som toe aan de TextView
             som.setText(random1 + " x " + random2 + " = ");
+            // bereken het antwoord
+            antwoord = random1 * random2;
+            // zet de antwoorden in een array
+            int[] opties = {antwoord, randombutton1, randombutton2};
+            // haal een random waard euit de array
+            int rnd = new Random().nextInt(opties.length);
+            // hieronde om 3 checks te generen zodat het antwoord per button verschilt en voeg de waardes toe aan de buttons
+            if (rnd == antwoord) {
+                // zorg hiermee voor unieke waardes
+                if(randombutton1 == randombutton2 || randombutton1 == antwoord || randombutton2 == antwoord){
+                    randombutton1 += 3;
+                    randombutton2 += 3;
+                }
+                optie1.setText(""+rnd);
+                optie2.setText(""+randombutton1);
+                optie3.setText(""+randombutton2);
+            }else if(rnd == randombutton1) {
+                // zorg hiermee voor unieke waardes
+                if(randombutton1 == randombutton2 || randombutton1 == antwoord || randombutton2 == antwoord){
+                    randombutton1 += 2;
+                    randombutton2 += 2;
+                }
+                optie1.setText(""+randombutton1);
+                optie2.setText(""+antwoord);
+                optie3.setText(""+randombutton2);
+            }else{
+                // zorg hiermee voor unieke waardes
+                if(randombutton1 == randombutton2 || randombutton1 == antwoord || randombutton2 == antwoord){
+                    randombutton1 += 6;
+                    randombutton2 += 6;
+                }
+                optie1.setText(""+randombutton2);
+                optie2.setText(""+randombutton1);
+                optie3.setText(""+antwoord);
+            }
+
+            // click listener voor de buttons zodat iemand er op klikt
+            optie1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // check het goede antwoord
+                    if(Integer.parseInt((String) optie1.getText()) == antwoord){
+                        // in dat geval wordt de button groen en wordt er 1 bij aantalgoed opgeteld
+                        optie1.setBackgroundColor(Color.GREEN);
+                        aantalgoed++;
+                    }else{
+                        //als het antwoord niet goed is maak button rood en voeg 1 toe bij aantalfout
+                        optie1.setBackgroundColor(Color.RED);
+                        aantalfout++;
+                    }
+                    // disable de overige buttons en maak de next button visible
+                    optie2.setEnabled(false);
+                    optie3.setEnabled(false);
+                    next.setVisibility(View.VISIBLE);
+                }
+            });
+            optie2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(Integer.parseInt((String) optie2.getText()) == antwoord) {
+                        optie2.setBackgroundColor(Color.GREEN);
+                        aantalgoed++;
+                    }else{
+                        optie2.setBackgroundColor(Color.RED);
+                        aantalfout++;
+                    }
+                    next.setVisibility(View.VISIBLE);
+                    optie1.setEnabled(false);
+                    optie3.setEnabled(false);
+                }
+            });
+            optie3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(Integer.parseInt((String) optie3.getText()) == antwoord){
+                        optie3.setBackgroundColor(Color.GREEN);
+                        aantalgoed++;
+                    }else{
+                        optie3.setBackgroundColor(Color.RED);
+                        aantalfout++;
+                    }
+                    next.setVisibility(View.VISIBLE);
+                    optie1.setEnabled(false);
+                    optie2.setEnabled(false);
+                }
+            });
+
         }
 
     }
