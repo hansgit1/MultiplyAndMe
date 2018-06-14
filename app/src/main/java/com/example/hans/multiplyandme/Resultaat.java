c59af7d6fa489577354751bf22999d3eaa607fbc
Ramon Arents <s1102867@student.hsleiden.nl>
2018-06-14 16:55:37 +0200

    pair programming: Chart code toegevoegd in resultaatscherm.

--- app/src/main/java/com/example/hans/multiplyandme/Resultaat.java
+++ app/src/main/java/com/example/hans/multiplyandme/Resultaat.java
@@ -1,13 +1,86 @@
 package com.example.hans.multiplyandme;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+
+import java.util.ArrayList;
 
 public class Resultaat extends AppCompatActivity {
+
+    private PieChart mChart;
+    public static final int MAX_ECTS = 60;
+    public static int currentEcts = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultaat);
+
+        mChart = (PieChart) findViewById(R.id.chart);
+        Description description = new Description();
+        description.setText("rekendata");
+        mChart.setDescription(description);
+        mChart.setTouchEnabled(false);
+        mChart.setDrawSliceText(true);
+        mChart.getLegend().setEnabled(false);
+        mChart.setTransparentCircleColor(Color.rgb(130, 130, 130));
+        mChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
+
+        setData(0);
+
+        Button fab = (Button) findViewById(R.id.plusTweeTest);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentEcts < MAX_ECTS) {
+                    setData(currentEcts += 2);
+                } else {
+                    setData(currentEcts = 0);
+                }
+            }
+        });
+    }
+    private void setData(int aantal) {
+        currentEcts = aantal;
+        ArrayList<Entry> yValues = new ArrayList<>();
+        ArrayList<String> xValues = new ArrayList<>();
+
+        yValues.add(new Entry(aantal, 0));
+        xValues.add("Behaalde ECTS");
+
+        yValues.add(new Entry(60 - currentEcts, 1));
+        xValues.add("Resterende ECTS");
+
+        //  http://www.materialui.co/colors
+        ArrayList<Integer> colors = new ArrayList<>();
+        if (currentEcts <10) {
+            colors.add(Color.rgb(244,81,30));
+        } else if (currentEcts < 40){
+            colors.add(Color.rgb(235,0,0));
+        } else if  (currentEcts < 50) {
+            colors.add(Color.rgb(253,216,53));
+        } else {
+            colors.add(Color.rgb(67,160,71));
+        }
+        colors.add(Color.rgb(255,0,0));
+
+        PieDataSet dataSet = new PieDataSet(yValues, "ects");
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        mChart.setData(data); // bind dataset aan chart.
+        mChart.invalidate();  // Aanroepen van een redraw
+        Log.d("aantal =", ""+currentEcts);
     }
 }
